29cb64388321f53d7351ea0a827e953372cf822f
unknown <nurfadillahkomalasari212.com>
2024-03-15 11:03:31 +0700

    praktikum4

---
+++ praktikum4/admin/praktikum1.php
@@ -0,0 +1,17 @@
+<?php 
+require_once 'navbar.html';
+require_once 'sidebar.html';
+?>
+
+<h1>Praktikum1</h1>
+<?php
+// membuat variabel user 
+$name = "nurfadillah komalasari";
+$age = 18;
+$weight = 60;
+
+echo $name;
+echo "<br> saya berusia". $age;
+
+?>
+<?php require_once 'footer.html'; ?>
