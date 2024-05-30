d84f20603e07a1b5be1d5b757592b90e0f2f686c
nurfadillahkomalasari <nurfadillahkomalasari212@gmail.com>
2024-05-30 23:14:49 +0700

    laravel3

--- puskesmas/resources/views/components/layout.blade.php
+++ puskesmas/resources/views/components/layout.blade.php
@@ -28,8 +28,8 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Blank Page</h1>
+        <div class="col-sm-6">
+            <h1>{{$page_name}}</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
@@ -59,7 +59,7 @@
           </div>
         </div>
         <div class="card-body">
-          Start creating your amazing application!
+          {{ $page_content }}
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
--- puskesmas/routes/web.php
+++ puskesmas/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\contactController;
+use App\Http\Controllers\kelurahanController;
+use App\Http\Controllers\PasienController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,4 +26,8 @@ Route::get('/profile', function () {
 // Praktikum Laravel 2
 Route::get('/dashboard', [AdminController::class, 'index']);
 Route::get('/contact', [AdminController::class, 'index']);
+
+// Praktikum Laravel 3
+Route::get('/dashboard/kelurahan', [KelurahanController::class, 'index']);
+Route::get('/dashboard/pasien', [PasienController::class, 'index']);
 
